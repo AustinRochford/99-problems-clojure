df221cf19327458811293d0a3e370385e374d086
Austin Rochford <austin.rochford@gmail.com>
2013-10-08 19:53:26 -0500

    Solved Problem 1

--- src/ninety_nine_problems/lists.clj
+++ src/ninety_nine_problems/lists.clj
@@ -2,3 +2,11 @@
     (:require
         [clojure.contrib.core :refer :all])
     (:gen-class))
+
+(defn last'
+    [coll]
+    (let
+        [[head & tail] coll]
+        (if (empty? tail)
+            head
+            (recur tail))))
--- test/ninety_nine_problems/lists_test.clj
+++ test/ninety_nine_problems/lists_test.clj
@@ -2,3 +2,9 @@
     (:require
         [clojure.test :refer :all]
         [ninety-nine-problems.lists :refer :all]))
+
+(deftest last'-test
+    (is (= 3 (last' '(1 2 3)))))
+
+(deftest last'-test-empty
+    (is (nil? (last' '()))))
