5bab988b58e56fe366d54fa7dbb032a1a01e3e08
Austin Rochford <austin.rochford@gmail.com>
2013-10-11 08:36:09 -0500

    Solved Problem 50

--- src/ninety_nine_problems/logic_codes.clj
+++ src/ninety_nine_problems/logic_codes.clj
@@ -1,6 +1,7 @@
 (ns ninety-nine-problems.logic-codes
     (:require
-        [clojure.contrib.core :refer :all])
+        [clojure.contrib.core :refer :all]
+        [clojure.data.priority-map :refer :all])
     (:gen-class))
 
 (defn gray-code
@@ -11,3 +12,42 @@
             (concat
                 (map #(conj % 0) code)
                 (map #(conj % 1) (reverse code))))))
+
+(defn huffman-step
+    [[fst p1] [snd p2]]
+    [`(nil ~fst ~snd) (+ p1 p2)])
+
+(defn pop'
+    [n queue]
+    [(take n queue) (nth (iterate pop queue) n)])
+
+(defn huffman-tree
+    [queue]
+    (if (= 1 (count queue))
+        (peek queue)
+        (let [[[fst snd] queue'] (pop' 2 queue)]
+            (->>
+                (huffman-step fst snd)
+                (conj queue')
+                (recur)))))
+
+(defn huffman-code-from-tree
+    [tree]
+    (if (not (seq? tree))
+        (hash-map tree '())
+        (let
+            [[_ left right] tree
+            lefts           (huffman-code-from-tree left)
+            rights          (huffman-code-from-tree right)]
+            (merge
+                (->>
+                    (map #(vector (first %) (conj (second %) 0)) lefts)
+                    (into {}))
+                (->>
+                    (map #(vector (first %) (conj (second %) 1)) rights)
+                    (into {}))))))
+
+(defn huffman-code
+    [freqs]
+    (let [[tree _] (huffman-tree freqs)]
+        (huffman-code-from-tree tree)))
--- test/ninety_nine_problems/logic_codes_test.clj
+++ test/ninety_nine_problems/logic_codes_test.clj
@@ -1,6 +1,7 @@
 (ns ninety-nine-problems.logic-codes-test
     (:require
         [clojure.test :refer :all]
+        [clojure.data.priority-map :refer :all]
         [ninety-nine-problems.logic-codes :refer :all]))
 
 (deftest gray-code-test-one
@@ -11,3 +12,32 @@
 
 (deftest gray-code-test-three
     (is (= '((0 0 0) (0 0 1) (0 1 1) (0 1 0) (1 1 0) (1 1 1) (1 0 1) (1 0 0)) (gray-code 3))))
+
+(deftest huffman-step-test
+    (is (= ['(nil :a :b) 3] (huffman-step [:a 2] [:b 1]))))
+
+(deftest pop'-test-two
+    (is (= ['([:b 1] [:c 3]) {:a 4 :d 6}] (pop' 2 (priority-map :a 4 :b 1 :c 3 :d 6)))))
+
+(deftest pop'-test-three
+    (is (= ['([:b 1] [:c 3] [:a 4]) {:d 6}] (pop' 3 (priority-map :a 4 :b 1 :c 3 :d 6)))))
+
+(deftest huffman-tree-test
+    (is (= ['(nil :b :a) 3] (huffman-tree (priority-map :a 2 :b 1)))))
+
+(deftest huffman-tree-test-three
+    (is (= ['(nil (nil :b :a) :c) 7] (huffman-tree (priority-map :a 2 :b 1 :c 4)))))
+
+(deftest huffman-tree-test-four
+    (is (= ['(nil (nil :a :b) (nil :c :d)) 22] (huffman-tree (priority-map :a 4 :b 5 :c 6 :d 7)))))
+
+(deftest huffman-code-from-tree-test
+    (is (= {:a '(1), :b '(0)} (huffman-code-from-tree '(nil :b :a)))))
+
+(deftest huffman-code-from-tree-test-three
+    (is (= {:c '(1), :a '(0 1), :b '(0 0)} (huffman-code-from-tree '(nil (nil :b :a) :c)))))
+
+(deftest huffman-code-test
+    (is (=
+        {:a '(0), :b '(1, 0, 1), :c '(1, 0, 0), :d '(1, 1, 1), :e '(1, 1, 0, 1), :f '(1, 1, 0, 0)}
+        (huffman-code (priority-map :a 45 :b 13 :c 12 :d 16 :e 9 :f 5)))))
