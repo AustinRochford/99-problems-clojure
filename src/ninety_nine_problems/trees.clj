dc3ec8753fdbc95678f46f0844ed673a4cd5ae9f
Austin Rochford <austin.rochford@gmail.com>
2013-10-11 20:18:01 -0500

    Solved Problem 56

--- src/ninety_nine_problems/trees.clj
+++ src/ninety_nine_problems/trees.clj
@@ -10,3 +10,25 @@
         (if (= 3 (count tree))
             (let [[_ left right] tree]
                 (and (tree? left) (tree? right))))))
+
+; Return to problem 55
+
+(defn same-tree?
+    [tree1 tree2]
+    (if (and (nil? tree1) (nil? tree2))
+        true
+    (if (or (nil? tree1) (nil? tree2))
+        false
+        (let
+            [[_ left1 right1] tree1
+            [_ left2 right2] tree2]
+            (and
+                (same-tree? left1 left2)
+                (same-tree? right1 right2))))))
+
+(defn symmetric?
+    [tree]
+    (or
+        (nil? tree)
+        (let [[_ left right] tree]
+            (same-tree? left right))))
--- test/ninety_nine_problems/trees_test.clj
+++ test/ninety_nine_problems/trees_test.clj
@@ -8,3 +8,42 @@
 
 (deftest tree?-test-false
     (is (not (tree? [:a [:b nil nil]]))))
+
+(deftest same-tree?-test-nil-true
+    (is (same-tree? nil nil)))
+
+(deftest same-tree?-test-first-nil-false
+    (is (not (same-tree? nil [:x nil nil]))))
+
+(deftest same-tree?-test-second-nil-false
+    (is (not (same-tree? [:x nil nil] nil))))
+
+(deftest same-tree?-test-leaves-true
+    (is (same-tree? [:x nil nil] [:y nil nil])))
+
+(deftest same-tree-test-true-two
+    (is (same-tree? [:x [:y nil nil] nil] [:a [:b nil nil] nil])))
+
+(deftest same-tree-test-false-two
+    (is (not (same-tree? [:x [:y nil nil] nil] [:x nil [:y nil nil]]))))
+
+(deftest symmetric?-nil
+    (is (symmetric? nil)))
+
+(deftest symmetric?-one
+    (is (symmetric? [:x nil nil])))
+
+(deftest symmetric?-two
+    (is (not (symmetric? [:x [:y nil nil] nil]))))
+
+(deftest symmetric?-three
+    (is (symmetric? [:x [:y nil nil] [:z nil nil]])))
+
+(deftest symmetric?-four
+    (is (not (symmetric? [:x [:y nil nil] [:z nil [:a nil nil]]]))))
+
+(deftest symmetric?-true-five
+    (is (symmetric? [:x [:y nil [:z nil nil]] [:a nil [:b nil nil]]])))
+
+(deftest symmetric?-false-five
+    (is (not (symmetric? [:x [:y nil [:z nil nil]] [:a [:b nil nil] nil]]))))
